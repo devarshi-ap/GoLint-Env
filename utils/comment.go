3ef56506187208c9138dbc9f0c758f16c4280bd8
Dev Patel <devarshi.ap@gmail.com>
2022-12-20 18:08:08 -0500

    refactor comments

--- utils/comment.go
+++ utils/comment.go
@@ -6,30 +6,7 @@ import (
 	"regexp"
 )
 
-/*
-Incorrectly-Spaced Comments - comments should be preceded with a space
-	❌ Wrong
-	#comment
-	✅ Correct
-	# comment
-
-Trailing Whitespace - detects if a line has trailing whitespace
-	❌ Wrong
-	# comment<whitespace>
-
-	✅ Correct
-	# comment
-
-Empty Comment (unnecessary) - detects if line only has '#'
-	❌ Wrong
-	#
-	#<whitespace>
-
-	✅ Correct
-	# <non-whitespace>
-*/
-
-func Validate(comment string, line int) {
+func ValidateComment(comment string, line int) {
 	if isCommentEmpty(comment) {
 		var err = Error{
 			name: "EmptyComment",
@@ -48,30 +25,49 @@ func Validate(comment string, line int) {
 		Error_Stack.Push(err);
 	}
 
-	if isCommentIncorrectlySpaced(comment) {
+	if !isCommentCorrectlySpaced(comment) {
 		var err = Error {
 			name: "CommentIncorrectlySpaced",
-			description: "<comment must follow [#<space><non-space>] format>",
+			description: "<comment must follow [<#><space><non-space>] format>",
 			line: line,
 		}
 		Error_Stack.Push(err);
 	}
 }
 
+
+/*
+Empty Comment (unnecessary) - detects if line only has '#'
+Wrong:			#,
+				#<whitespace>
+Correct: 		# <non-whitespace>
+*/
 func isCommentEmpty(comment string) bool {
 	return len(strings.TrimSpace(comment)) == 1
 }
 
+
+/*
+Trailing Whitespace - detects if a line has trailing whitespace
+Wrong:			# comment<whitespace>
+Correct:		# comment
+*/
 func commentHasTrailingSpace(comment string) bool {
 	lastChar := comment[len(comment) - 1];
 	return unicode.IsSpace(rune(lastChar));
 }
 
-func isCommentIncorrectlySpaced(comment string) bool {
+
+/*
+Incorrectly-Spaced Comments - comments should be preceded with a space
+Wrong:			#comment
+Correct:		# comment
+*/
+func isCommentCorrectlySpaced(comment string) bool {
 	if isCommentEmpty(comment) {
-		return false;
+		return true;
 	} else {
-		match, _ := regexp.MatchString(`(^#[ ][^ ]+$)`, comment);
-		return !match;
+		match, _ := regexp.MatchString(`#[ ]{1}[^ ]{1,}`, comment);
+		return match;
 	}
 }
