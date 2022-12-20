53bca9219b750e0d0b301e457ad8aea223c6d91d
Dev Patel <devarshi.ap@gmail.com>
2022-12-20 14:32:48 -0500

    precheck parameters

--- utils/error.go
+++ utils/error.go
@@ -9,7 +9,13 @@ type Error struct {
 
 // NewError: Creates new error object and returns its address (to be assigned to variable)
 func NewError(ename, edesc string, eline int) *Error {
-    return &Error{
+	if eline < 0 {
+		fmt.Printf("<Line of Error (%d) cannot be Negative>\n\n", eline);
+	} else if len(ename) == 0 || len(edesc) == 0 {
+		fmt.Printf("<Name of Error/Desc. cannot be Empty>\n\n");
+	}
+
+	return &Error{
 		name: ename,
 		description: edesc,
 		line: eline,
@@ -18,5 +24,5 @@ func NewError(ename, edesc string, eline int) *Error {
 
 // PrintError: prints formatted error [.env:<line> <error>: <error-msg>]
 func PrintError(e Error) {
-	fmt.Printf(".env:%d %s: %s\n", e.line, e.name, e.description);
+	fmt.Printf(".env:%d\n\t%s:\t%s\n", e.line, e.name, e.description);
 }
