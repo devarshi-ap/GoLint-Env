c2e3bb88e259a129d67426c24bb54bfcb62fa692
Dev Patel <devarshi.ap@gmail.com>
2022-12-23 15:15:38 -0500

    comment out unused colors

--- utils/colors.go
+++ utils/colors.go
@@ -1,10 +1,10 @@
 package utils;
 
-var colorReset string = "\033[0m";
-var colorRed string = "\033[31m";
-var colorGreen string = "\033[32m";
+// var colorReset string = "\033[0m";
+// var colorRed string = "\033[31m";
+// var colorGreen string = "\033[32m";
 var colorYellow string = "\033[33m";
-var colorBlue string = "\033[34m";
+// var colorBlue string = "\033[34m";
 var colorPurple string = "\033[35m";
-var colorCyan string = "\033[36m";
-var colorWhite string = "\033[37m";
+// var colorCyan string = "\033[36m";
+// var colorWhite string = "\033[37m";
